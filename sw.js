92e018238f304e91ec017b4a09b68a3239a87b55
Tiger Oakes <tigeroakes@gmail.com>
2016-11-26 01:02:28 -0800

    Working on service worker

--- sw.js
+++ sw.js
@@ -1,14 +1,32 @@
 const CACHE = 'cache-and-update';
+// https://serviceworke.rs/strategy-cache-and-update_service-worker_doc.html
 
 function precache() {
-	return caches.open(CACHE).then(cache => cache.addAll([
-		'./index.html'
-	]));
+	return caches.open(CACHE).then((cache) => {
+		cache.addAll([
+			'./images/contact/white-github.svg',
+			'./images/contact/white-twitter.svg',
+			'./images/contact/white-linkedin.svg',
+			/*'./images/big-island-buses/logo.png',
+			'./images/latch-on/logo.png',
+			'./images/mbta-energy/logo.png',
+			'./images/oml-contracting/logo.svg',
+			'./images/ubc-farm/logo.png',
+			'./images/unity-polygon/logo.png',*/
+			'./images/profile-4x.jpg',
+		]);
+		return cache.addAll([
+			'./',
+			'./projects/',
+			'./resume/',
+			'./contact/',
+		]);
+	});
 }
 
 function fromCache(request) {
 	return caches.open(CACHE).then(cache =>
-		fetch(request).then(response => cache.put(request, response))
+		cache.match(request).then(match => match || Promise.reject('no-match'))
 	);
 }
 
