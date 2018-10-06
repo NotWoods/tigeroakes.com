81f0dac9a54f0f16e3f78637d5cec99b1cbb563d
Tiger Oakes <tigeroakes@gmail.com>
2018-10-06 13:10:13 -0700

    Fix service worker globs

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,13 +2,13 @@
 /** @type {import('sw-precache').Options} */
 const config = {
   staticFileGlobs: [
-    "profile_*_160x*.jpg",
-    "hero.jpg",
-    "**/*.css",
-    "**/*.html",
-    "**/*.svg",
-    "contact/background*.jpg",
-    "projects/*/logo_*_128x*.png"
+    "public/profile_*_160x*.jpg",
+    "public/hero.jpg",
+    "public/**/*.css",
+    "public/**/*.html",
+    "public/**/*.svg",
+    "public/contact/background*.jpg",
+    "public/projects/*/logo_*_128x*.png"
   ]
 };
 
