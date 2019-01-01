5c3f6f380c8f4e363a49115aad1ece8ea8625b40
Tiger Oakes <contact@tigeroakes.com>
2018-12-31 21:12:59 -0800

    Update service worker

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,15 +1,29 @@
 // @ts-check
+
+/** @type {import('sw-precache').Handler} */
+const handler = 'fastest'
+
 /** @type {import('sw-precache').Options} */
 const config = {
-  staticFileGlobs: [
-    "public/profile_*_160x*.jpg",
-    "public/hero.jpg",
-    "public/**/*.css",
-    "public/**/*.html",
-    "public/**/*.svg",
-    "public/projects/*/*.jpg",
-    "public/projects/*/*.png",
-  ]
+    cacheId: 'tigeroakes',
+    staticFileGlobs: [
+        'public/profile_*_160x*.jpg',
+        'public/index.html',
+        'public/@(contact|resume)/index.html',
+        'public/@(featured-in|posts|projects)/index.html',
+        'public/@(featured-in|posts|projects)/*/*.html',
+        'public/**/*.css',
+        'public/contact/*.svg',
+        'public/projects/*/logo.svg',
+        'public/projects/*/logo_*_128x*.png',
+        'public/js/service-worker-registration.min.js'
+    ],
+    runtimeCaching: [
+        /\/(projects|posts)\/\*\/\*.(jpg|png)/,
+        '/hero.jpg',
+        '/contact/background.jpg',
+        '/featured-in/index.html'
+    ].map(urlPattern => ({ urlPattern, handler }))
 };
 
 module.exports = config;
