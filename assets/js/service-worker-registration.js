c85bd91aac47bb7e67af3d8071b2ef11391ab313
Tiger Oakes <contact@tigeroakes.com>
2020-08-11 08:43:20 -0700

    Move service worker to all pages

--- assets/js/service-worker-registration.js
+++ assets/js/service-worker-registration.js
@@ -31,7 +31,7 @@ if ('serviceWorker' in navigator) {
     // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
     // See https://github.com/slightlyoff/ServiceWorker/issues/468
     navigator.serviceWorker
-      .register('service-worker.js')
+      .register('/service-worker.js')
       .then(reg => {
         // updatefound is fired if service-worker.js changes.
         reg.onupdatefound = () => {
