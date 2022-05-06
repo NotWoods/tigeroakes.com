5c1ab941ee67bfbe73f683e2aa14651daf3c71d7
Tiger Oakes <contact@tigeroakes.com>
2022-05-05 17:23:56 -0700

    Add featured projects

---
+++ src/scripts/projects.ts
@@ -0,0 +1,25 @@
+interface Link {
+  title: string;
+  github?: string;
+  link?: string;
+}
+
+export function projectButtons(links: readonly Link[], pageUrl: string | URL) {
+  return links?.map((link) => ({
+    title: link.title,
+    href: buttonLink(link, pageUrl),
+  }));
+}
+
+function buttonLink(link: Link, pageUrl: string | URL) {
+  switch (link.title) {
+    case 'Details':
+    case 'Case study':
+      return pageUrl;
+    default:
+      if (link.github) {
+        return `https://github.com/${link.github}`;
+      }
+  }
+  return link.link;
+}
