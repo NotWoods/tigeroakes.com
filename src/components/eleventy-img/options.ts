a21bd84416efb6888715703d92befb99f1d354ea
Tiger Oakes <contact@tigeroakes.com>
2022-05-16 18:18:59 -0700

    Adding types for 11ty image

--- src/components/eleventy-img/options.ts
+++ src/components/eleventy-img/options.ts
@@ -1,16 +1,17 @@
+import type { ImageOptions } from '@11ty/eleventy-img';
 import { basename, dirname, extname } from 'path';
 
 export function parentFolder(src: string) {
   return basename(dirname(src));
 }
 
-export const imageOptions = {
+export const imageOptions: Partial<ImageOptions> = {
   outputDir: `${import.meta.env.PROD ? 'dist' : 'static'}/assets/images`,
   urlPath: '/assets/images',
   sharpWebpOptions: { quality: 90 },
   sharpAvifOptions: { quality: 90 },
-  filenameFormat(id: string, src: string, width: string, format: string) {
+  filenameFormat(id, src, width, format) {
     const fileName = basename(src, extname(src));
     return `${parentFolder(src)}-${fileName}-${width}.${id}.${format}`;
-  }
+  },
 };
