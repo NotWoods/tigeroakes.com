54624a30dc605a7e6c0e22eacfe13355e6ea4a77
Tiger Oakes <contact@tigeroakes.com>
2022-11-22 09:59:18 +0800

    Add webfinger

---
+++ netlify/edge-functions/webfinger.ts
@@ -0,0 +1,45 @@
+import type { Context } from "https://edge.netlify.com";
+
+const mastodon_server = "https://hachyderm.io/";
+const mastodon_user = "notwoods";
+
+const resources = new Set([
+  "acct:notwoods@tigeroakes.com",
+  "acct:contact@tigeroakes.com",
+  "acct:tiger@tigeroakes.com",
+  "acct:tigeroakes@tigeroakes.com",
+]);
+
+export default function webfinger(request: Request, context: Context) {
+  const resource = new URL(request.url).searchParams.get("resource");
+
+  if (!resources.has(resource)) {
+    return new Response("no such resource", {
+      status: 404,
+    });
+  }
+
+  return context.json({
+    subject: resource,
+    aliases: [
+      `https://${mastodon_server}/@${mastodon_user}`,
+      `https://${mastodon_server}/users/${mastodon_user}`,
+    ],
+    links: [
+      {
+        rel: "http://webfinger.net/rel/profile-page",
+        type: "text/html",
+        href: `https://${mastodon_server}/@${mastodon_user}`,
+      },
+      {
+        rel: "self",
+        type: "application/activity+json",
+        href: `https://${mastodon_server}/users/${mastodon_user}`,
+      },
+      {
+        rel: "http://ostatus.org/schema/1.0/subscribe",
+        template: `https://${mastodon_server}/authorize_interaction?uri={uri}`,
+      },
+    ],
+  });
+}
